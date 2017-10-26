63b14bf4ce0fae6f2a45bddeebd4da9b206f6e69
Gary Pendergast <gary@pento.net>
2017-10-26 23:37:06 +1100

    Improve next/previous navigation

--- single.php
+++ single.php
@@ -21,7 +21,10 @@ get_sidebar( 'handbook' );
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'prev_text' => '← %title',
+				'next_text' => '%title →',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
