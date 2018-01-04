e3956e19bd07c5e83b85d9bced6ced96e49a9720
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2018-01-04 12:17:37 -0800

    Remove #primary container (superfluous wrapping container for float-based layouts) and give <main> the #primary ID. Redirect skip-to-content to target #primary.

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'gutenbergtheme' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'gutenbergtheme' ); ?></a>
 		<header id="masthead" class="site-header">
 			<div class="site-branding">
 				<?php
