83ec8912192cfe7fd5e27cf142302fc4b521f2e9
Amor <contact@amorkumar.com>
2021-11-13 00:39:11 -0500

    Disable fullscreen mode default

--- inc/legible-gutenberg.php
+++ inc/legible-gutenberg.php
@@ -17,3 +17,11 @@ function gutenberg_starter_theme_allowed_block_types( $allowed_blocks ) {
 }
 add_filter( 'allowed_block_types', 'gutenberg_starter_theme_allowed_block_types' );
 
+/**
+ * Disable Gutenberg's default fullscreen mode.
+ */
+function gutenberg_starter_theme_disable_editor_fullscreen_mode() {
+	$script = "window.onload = function() { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } }";
+	wp_add_inline_script( 'wp-blocks', $script );
+}
+add_action( 'enqueue_block_editor_assets', 'gutenberg_starter_theme_disable_editor_fullscreen_mode' );
