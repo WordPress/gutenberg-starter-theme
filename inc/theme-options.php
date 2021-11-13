6bea4c6c4d98577693924571198204ed6ead287e
Amor <contact@amorkumar.com>
2021-11-13 00:36:36 -0500

    Remove dark theme option

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Set up a WP-Admin page for managing turning on and off theme features.
@@ -25,7 +25,6 @@ function gutenberg_starter_theme_options() {
 	register_setting( 'gutenberg-starter-theme-options', 'gutenberg-starter-theme-align-wide', array( 'default' => 1 ) );
 	register_setting( 'gutenberg-starter-theme-options', 'gutenberg-starter-theme-wp-block-styles', array( 'default' => 1 ) );
 	register_setting( 'gutenberg-starter-theme-options', 'gutenberg-starter-theme-editor-color-palette', array( 'default' => 1 ) );
-	register_setting( 'gutenberg-starter-theme-options', 'gutenberg-starter-theme-dark-mode' );
 	register_setting( 'gutenberg-starter-theme-options', 'gutenberg-starter-theme-responsive-embeds', array( 'default' => 1 ) );
 }
 
@@ -62,15 +61,6 @@ function gutenberg_starter_theme_options_page() { ?>
 				<tr valign="top">
 					<td>
 						<label>
-							<input name="gutenberg-starter-theme-dark-mode" type="checkbox" value="1" <?php checked( '1', get_option( 'gutenberg-starter-theme-dark-mode' ) ); ?> />
-							<?php _e( 'Enable a dark theme style.', 'gutenberg-starter-theme' ); ?>
-							(<a href="https://developer.wordpress.org/block-editor/developers/themes/theme-support/#dark-backgrounds"><code>dark-editor-style</code></a>)
-						</label>
-					</td>
-				</tr>
-				<tr valign="top">
-					<td>
-						<label>
 							<input name="gutenberg-starter-theme-wp-block-styles" type="checkbox" value="1" <?php checked( '1', get_option( 'gutenberg-starter-theme-wp-block-styles' ) ); ?> />
 							<?php _e( 'Enable core block styles on the front end.', 'gutenberg-starter-theme' ); ?>
 							(<a href="https://developer.wordpress.org/block-editor/developers/themes/theme-support/#default-block-styles"><code>wp-block-styles</code></a>)
@@ -100,7 +90,7 @@ function gutenberg_starter_theme_options_page() { ?>
 function gutenberg_starter_theme_enable_align_wide() {
 
 	if ( get_option( 'gutenberg-starter-theme-align-wide', 1 ) == 1 ) {
-		
+
 		// Add support for full and wide align images.
 		add_theme_support( 'align-wide' );
 	}
@@ -113,7 +103,7 @@ add_action( 'after_setup_theme', 'gutenberg_starter_theme_enable_align_wide' );
  */
 function gutenberg_starter_theme_enable_editor_color_palette() {
 	if ( get_option( 'gutenberg-starter-theme-editor-color-palette', 1 ) == 1 ) {
-		
+
 		// Add support for a custom color scheme.
 		add_theme_support( 'editor-color-palette', array(
 			array(
@@ -141,41 +131,13 @@ function gutenberg_starter_theme_enable_editor_color_palette() {
 }
 add_action( 'after_setup_theme', 'gutenberg_starter_theme_enable_editor_color_palette' );
 
-
-/**
- * Enable dark mode if gutenberg-starter-theme-dark-mode setting is active.
- */
-function gutenberg_starter_theme_enable_dark_mode() {
-	if ( get_option( 'gutenberg-starter-theme-dark-mode' ) == 1 ) {
-		
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-		add_editor_style( 'style-editor-dark.css' );
-		
-		// Add support for dark styles.
-		add_theme_support( 'dark-editor-style' );
-	}
-}
-add_action( 'after_setup_theme', 'gutenberg_starter_theme_enable_dark_mode' );
-
-
-/**
- * Enable dark mode on the front end if gutenberg-starter-theme-dark-mode setting is active.
- */
-function gutenberg_starter_theme_enable_dark_mode_frontend_styles() {
-	if ( get_option( 'gutenberg-starter-theme-dark-mode' ) == 1 ) {
-		wp_enqueue_style( 'gutenberg-starter-themedark-style', get_template_directory_uri() . '/css/dark-mode.css' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'gutenberg_starter_theme_enable_dark_mode_frontend_styles' );
-
 /**
  * Enable core block styles support if the gutenberg-starter-theme-wp-block-styles setting is active.
  */
 function gutenberg_starter_theme_enable_wp_block_styles() {
 
 	if ( get_option( 'gutenberg-starter-theme-wp-block-styles', 1 ) == 1 ) {
-		
+
 		// Adding support for core block visual styles.
 		add_theme_support( 'wp-block-styles' );
 	}
@@ -189,7 +151,7 @@ add_action( 'after_setup_theme', 'gutenberg_starter_theme_enable_wp_block_styles
 function gutenberg_starter_theme_enable_responsive_embeds() {
 
 	if ( get_option( 'gutenberg-starter-theme-responsive-embeds', 1 ) == 1 ) {
-		
+
 		// Adding support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
 	}
